4a5adaae881398c5df9e9d9ac12e616d48ac56db
shivamsharma-1996 <shivamsharma707@ahoocom>
2018-10-25 17:29:45 +0530

    this project provide multi screen support(single pane ui for handset & multipane ui for tablets)

---
+++ app/src/main/java/shivam/com/multile_fragments/MainActivity.java
@@ -0,0 +1,87 @@
+package shivam.com.multile_fragments;
+
+import android.os.Bundle;
+import android.support.annotation.NonNull;
+import android.support.annotation.Nullable;
+import android.support.design.widget.NavigationView;
+import android.support.v4.app.FragmentManager;
+import android.support.v4.view.GravityCompat;
+import android.support.v4.widget.DrawerLayout;
+import android.support.v7.app.ActionBar;
+import android.support.v7.app.AppCompatActivity;
+import android.support.v7.widget.Toolbar;
+import android.view.MenuItem;
+
+
+public class MainActivity extends AppCompatActivity implements MasterFragment.Callbacks {
+    private static final String TAG_MASTER_FRAGMENT = "TAG_MASTER_FRAGMENT";
+    private static final String TAG_DETAIL_FRAGMENT = "TAG_DETAIL_FRAGMENT";
+    private DrawerLayout drawerLayout;
+
+    @Override
+    protected void onCreate(@Nullable Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+
+        setContentView(R.layout.activity_main);
+
+        // setup toolbar
+        Toolbar toolbar = (Toolbar) findViewById(R.id.toolbar);
+        setSupportActionBar(toolbar);
+
+        // setup drawer view
+        drawerLayout = (DrawerLayout) findViewById(R.id.drawer_layout);
+        if (drawerLayout != null) {
+            NavigationView navigationView = (NavigationView) findViewById(R.id.master_fragment_container);
+            navigationView.setNavigationItemSelectedListener(new NavigationView.OnNavigationItemSelectedListener() {
+                @Override
+                public boolean onNavigationItemSelected(@NonNull MenuItem item) {
+                    return true;
+                }
+            });
+
+            // setup menu icon
+            final ActionBar actionBar = getSupportActionBar();
+            if (actionBar != null) {
+                actionBar.setHomeAsUpIndicator(R.drawable.ic_menu_black_24dp);
+                actionBar.setDisplayHomeAsUpEnabled(true);
+            }
+        }
+
+        // insert detail fragment into detail container
+        DetailFragment detailFragment = DetailFragment.newInstance();
+        FragmentManager fragmentManager = getSupportFragmentManager();
+        fragmentManager.beginTransaction()
+                .add(R.id.detail_fragment_container, detailFragment, TAG_DETAIL_FRAGMENT)
+                .commit();
+
+        // insert master fragment into master container (i.e. nav view)
+        MasterFragment masterFragment = MasterFragment.newInstance();
+        fragmentManager.beginTransaction()
+                .add(R.id.master_fragment_container, masterFragment, TAG_MASTER_FRAGMENT)
+                .commit();
+    }
+
+    @Override
+    public boolean onOptionsItemSelected(MenuItem item) {
+        // The action bar home/up action should open or close the drawer.
+        switch (item.getItemId()) {
+            case android.R.id.home:
+                drawerLayout.openDrawer(GravityCompat.START);
+                return true;
+        }
+
+        return super.onOptionsItemSelected(item);
+    }
+
+    @Override
+    public void onMasterItemClicked(int masterItemId) {
+        DetailFragment detailFragment = (DetailFragment) getSupportFragmentManager()
+                .findFragmentByTag(TAG_DETAIL_FRAGMENT);
+        detailFragment.onMasterItemClicked(masterItemId);
+
+        // Close the navigation drawer
+        if (drawerLayout != null) {
+            drawerLayout.closeDrawers();
+        }
+    }
+}
